6a14248cba718210288f895ac4875bdf8e3ec2a3
bestarkam <bestarkam1234@gmail.com>
2025-07-18 16:17:43 +0100

    personalisation 1 de l'interface admin

--- app_sk/admin.py
+++ app_sk/admin.py
@@ -8,3 +8,10 @@ admin.site.register(DetailService)
 admin.site.register(DemanderServicer)
 admin.site.register(Temoignage)
 admin.site.register(Contact)
+
+
+#personnaliser mon interface admin
+admin.site.site_header = "Administration de vezaservice"
+admin.site.site_title = "Veza administration"
+admin.site.index_title = "Bienvenue dans l'administration"
+
